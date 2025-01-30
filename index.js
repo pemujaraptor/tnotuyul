0870bd2df586ac70cdcdb0d892a564accad2a633
recitativonika <fool-body-cheddar@duck.com>
2025-01-30 19:14:17 +0700

    Fix API error

--- index.js
+++ index.js
@@ -413,17 +413,29 @@ async function getUserId(index) {
 
   try {
     const response = await axios.post(loginUrl, {
-      email: accounts[index].email,
-      password: accounts[index].password
-    }, {
-      httpsAgent: agent,
-      headers: {
-        'Authorization': `Bearer ${accessTokens[index]}`,
-        'Content-Type': 'application/json',
-        'authority': 'auth.teneo.pro',
-        'x-api-key': 'OwAG3kib1ivOJG4Y0OCZ8lJETa6ypvsDtGmdhcjA'
-      }
-    });
+    email: accounts[index].email,
+    password: accounts[index].password
+  }, {
+    httpsAgent: agent,
+    headers: {
+      'Authorization': `Bearer ${accessTokens[index]}`,
+      'Content-Type': 'application/json',
+      'authority': 'auth.teneo.pro',
+      'x-api-key': 'OwAG3kib1ivOJG4Y0OCZ8lJETa6ypvsDtGmdhcjB',
+      'accept': 'application/json, text/plain, */*',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'en-US,en;q=0.9,id;q=0.8',
+      'origin': 'https://dashboard.teneo.pro',
+      'referer': 'https://dashboard.teneo.pro/',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"'
+    }
+  });
 
     const { user, access_token } = response.data;
     userIds[index] = user.id;
